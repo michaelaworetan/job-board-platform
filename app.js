7e1656280e7f5b7497782db78d3edc3c038572cd
Aworetan Michael <mvillacs07@gmail.com>
2024-07-03 00:05:36 +0100

    modifed app entry

--- app.js
+++ app.js
@@ -20,4 +20,15 @@ app.use(express.json());
 // app.use('/api/jobs', jobRoutes);
 // app.use('/api/applications', applicationRoutes);
 
+// Root endpoint for health check
+app.get('/', (req, res) => {
+    res.send('job-board-platform API is running');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: err.message });
+});
+
 module.exports = app;
